fd7db0bd7b74a9072ee044000a886815263c7630
Jennifer Hightower <jenniferhightower2020@gmail.com>
2022-06-09 08:12:50 -0500

    update to questions 16-18

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -74,6 +74,18 @@ if (pets < allowedPets) {
 // assigns the highest value to `mostPets`. There's several possibilities --
 // be sure to think through all the scenarios. 
 // console.log `mostPets` after the conditional has run.
+let mostPets;
+
+if (pets > friendsPets) {
+    mostPets = pets;
+} else if (friendsPets > pets) {
+    mostPets = friendsPets;
+} else if (pets === friendsPets) {
+    mostPets = pets;
+    console.log('My friend and I have the same number of pets.');
+}
+
+console.log(mostPets);
 
 // 17 - Write a *switch* statement that logs:
 //      "First is the worst" if your lucky number is 1
@@ -81,7 +93,20 @@ if (pets < allowedPets) {
 //      "Third is the one with the polka dot dress" if your lucky number is 3
 //      Otherwise, log "Luck is what happens when preparation meets opportunity"
 //      You'll need to research how to use switch statements!
+luckyNumber = 3;
+switch (luckyNumber) {
+    case 1:
+        console.log('First is the worst.');
+        break;
+    case 2:
+        console.log('Second is the best.');
+        break;
+    case 3: 
+        console.log('Third is the one with the polka dot dress.');
+        break;
+    default:
+        console.log('Luck is what happens when preparation meets opportunity.');
+}
 
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
-
-
+adventurous ? console.log('Adventures are great!') : console.log('How about we stay home?');
